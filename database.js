7bea5fc555421883e9b4947a92c2716948c064ef
GustavoFavorito <gustavofavorito.d@gmail.com>
2024-03-09 12:20:40 -0300

    Ajustando alguns parametros e removendo comentarios

--- database.js
+++ database.js
@@ -6,13 +6,11 @@ var config = {
     password: process.env.DB_PASS,
 };
 
-// Later on when running from Google Cloud, env variables will be passed in container cloud connection config
 if(process.env.NODE_ENV === 'production') {
   console.log('Running from cloud. Connecting to DB through GCP socket.');
   config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
 }
 
-// When running from localhost, get the config from .env
 else {
   console.log('Running from localhost. Connecting to DB directly.');
   config.host = process.env.DB_HOST;
--- index.js
+++ index.js
@@ -37,7 +37,6 @@ app.get('/fetch-ingrediente', (req, res) => {
   );
 });
 
-// Use port 8080 by default, unless configured differently in Google Cloud
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
    console.log(`App is running at: http://localhost:${port}`);
